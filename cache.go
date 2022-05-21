7a18909206d0872ccb6f07d0bf6b8a3f217f0b07
vital <vitalikgk@mail.ru>
2022-05-21 09:22:19 +0400

    test

--- cache.go
+++ cache.go
@@ -7,18 +7,18 @@ import (
 type Cache struct {
 	key      string
 	value    string
-	stings   string
+	stings   time.Time
 	deadline time.Time
 }
 
 type MapCache map[string]Cache
 
-func NewCache(key, value string, deadline time.Time) Cache {
+func NewCache(key, value string, deadline, stings time.Time) Cache {
 
 	return Cache{
-		key:   key,
-		value: value,
-		//	stings:   stings,
+		key:      key,
+		value:    value,
+		stings:   stings,
 		deadline: deadline,
 	}
 }
@@ -30,16 +30,16 @@ func (m MapCache) Get(key string) (string, bool) {
 	// } else {
 	// 	fmt.Printf("Элемент найден (%s , %s, %s)", k.key, k.value, k.stings)
 	// }
-	return k.key + k.value + k.stings, ok
+	return k.key + k.value + k.stings.Format("Jan _2 15:04:05.000000"), ok
 }
 
 func (m MapCache) Put(key, value string) {
 	k, ok := m[key]
 	if ok {
-		k = NewCache(key, value, time.Date(1, time.January, 1, 00, 0, 0, 0, time.UTC))
+		k = NewCache(key, value, k.stings, time.Date(1, time.January, 1, 00, 0, 0, 0, time.UTC))
 		// fmt.Println("Изменили мапу ", key)
 	} else {
-		k = NewCache(key, value, time.Date(1, time.January, 1, 00, 0, 0, 0, time.UTC))
+		k = NewCache(key, value, time.Now(), time.Date(1, time.January, 1, 00, 0, 0, 0, time.UTC))
 		// fmt.Println("Добавили мапу ", key)
 	}
 	m[key] = k
@@ -52,7 +52,7 @@ func (m MapCache) Keys() []string {
 		k, ok := m[key]
 		if ok {
 			if k.deadline != time.Date(1, time.January, 1, 00, 0, 0, 0, time.UTC) {
-				listCache = append(listCache, k.key+", "+k.stings+", "+k.deadline.Format("15:04:05"))
+				listCache = append(listCache, k.key+", "+k.value+", "+k.stings.Format("Jan _2 15:04:05.000000")+", "+k.deadline.Format("15:04:05"))
 			}
 		}
 

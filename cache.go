11761e647acddfee22588065758f215045ca58df
vital <vitalikgk@mail.ru>
2022-05-21 04:48:20 +0400

    test

--- cache.go
+++ cache.go
@@ -1,23 +1,62 @@
 package cache
 
-import "time"
+import (
+	"time"
+)
 
 type Cache struct {
+	key      string
+	value    string
+	stings   string
+	deadline time.Time
 }
 
-func NewCache() Cache {
-	return Cache{}
+type MapCache map[string]Cache
+
+func NewCache(key, value, stings string, deadline time.Time) Cache {
+
+	return Cache{
+		key:      key,
+		value:    value,
+		stings:   stings,
+		deadline: deadline,
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (m MapCache) Get(key string) (string, bool) {
+	k, ok := m[key]
+	// if !ok {
+	// 	fmt.Println("Элемента нет в списке")
+	// } else {
+	// 	fmt.Printf("Элемент найден (%s , %s, %s)", k.key, k.value, k.stings)
+	// }
+	return k.key + k.value + k.stings, ok
+}
+
+func (m MapCache) Put(key, value string) {
+	k, ok := m[key]
+	if ok {
+		k = NewCache(key, value, k.stings, time.Date(1, time.January, 1, 00, 0, 0, 0, time.UTC))
+		// fmt.Println("Изменили мапу ", key)
+	} else {
+		k = NewCache(key, value, time.Now().Format("Jan _2 15:04:05.000000"), time.Date(1, time.January, 1, 00, 0, 0, 0, time.UTC))
+		// fmt.Println("Добавили мапу ", key)
+	}
+	m[key] = k
 
 }
 
-func (receiver) Put(key, value string) {
-}
+func (m MapCache) Keys() []string {
+	var listCache []string
+	for key, _ := range m {
+		k, ok := m[key]
+		if ok {
+			if k.deadline != time.Date(1, time.January, 1, 00, 0, 0, 0, time.UTC) {
+				listCache = append(listCache, k.key+", "+k.value+", "+k.stings+", "+k.deadline.Format("15:04:05"))
+			}
+		}
 
-func (receiver) Keys() []string {
-}
-
-func (receiver) PutTill(key, value string, deadline time.Time) {
+	}
+	// fmt.Println("Keys = ", listCache)
+	return listCache
 }
